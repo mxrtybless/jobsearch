49d141cea82cc902aac3b4d0935bafe167be935d
Mxrty <ruam1nske@gmail.com>
2026-08-18 15:58:53 +0300

    add RespondedApplicantRepository

--- src/main/java/kg/attractor/jobsearch/repository/RespondedApplicantRepository.java
+++ src/main/java/kg/attractor/jobsearch/repository/RespondedApplicantRepository.java
@@ -1,8 +1,28 @@
 package kg.attractor.jobsearch.repository;
 
 import kg.attractor.jobsearch.model.RespondedApplicant;
+import kg.attractor.jobsearch.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface RespondedApplicantRepository
         extends JpaRepository<RespondedApplicant, Integer> {
+
+    boolean existsByResume_IdAndVacancy_Id(
+            Integer resumeId,
+            Integer vacancyId
+    );
+
+    @Query("""
+            select distinct response.resume.applicant
+            from RespondedApplicant response
+            where response.vacancy.id = :vacancyId
+            """)
+    List<User> findApplicantsByVacancyId(
+            @Param("vacancyId")
+            Integer vacancyId
+    );
 }
